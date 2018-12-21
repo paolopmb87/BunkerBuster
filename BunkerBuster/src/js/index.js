2269902a9c2573188ad2dd0f93ad37851cef49a0
pablomas <paolo.pmb@gmail.com>
2018-12-21 19:02:11 +0100

    no message

--- BunkerBuster/src/js/index.js
+++ BunkerBuster/src/js/index.js
@@ -14,67 +14,104 @@
 
 //THREEJS VAR
 
+var sceneWidth;
+var sceneHeight;
 var camera;
 var scene;
 var renderer;
-var light;
-var ambLight;
-
-var obs = [];
-var tank = null;
-var armor = null;
-var mine = null;
-var fuel_tank = null;
-var obstacle;
-var destroied = false;
-var paused = false;
-var power_up = false;
-var planeMesh =null;
-var height, width, fov, aspect, near, far;
-var animation;
-
-var NUM_LOADED = 0;
+var dom;
+var hero;
+var sun;
+var ground;
+var orbitControl;
+var tank;
 var TANK_LOADED = false;
+var NUM_LOADED = 0;
 
-height = 600;
-width = 900;
-
-fov = 45;
-aspect = width/height;
-near = 0.1; far = 10000;
-
-function init(){
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(width, height);
-
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.y = 5;
-  camera.position.z = 30;
-  light = new THREE.DirectionalLight(0xFFFFFF, 0.75);
-  light.position.set(0,200, 40);
-  scene = new THREE.Scene();
-  scene.add(camera);
-  scene.add(light);
-  ambLight = new THREE.AmbientLight(0x404040);
-  scene.add(ambLight);
-  document.querySelector('#game_ifrm').appendChild(renderer.domElement)
+function init() {
+  // set up the scene
+  createScene();
+  //call game loop
+  update();
 }
 
-function loadScene(){
-    planeMesh = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(400,550 ,32),
-    new THREE.MeshPhongMaterial({color: 0x085A14, side: THREE.DoubleSide})
-  )
-  planeMesh.rotation.x = -1.58
-  planeMesh.scale.set(5, 5)
-  planeMesh.position.set(0,0,0)
-  scene.add(planeMesh)
+function createScene(){
+  scene = new THREE.Scene();//the 3d scene
+
+  sceneWidth=window.innerWidth;
+  console.log(sceneWidth)
+  sceneHeight=window.innerHeight;
+  //scene.fog = new THREE.Fog(0x00ff00, 50, 800);//enable fog
+  camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
+  renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
+  renderer.shadowMap.enabled = true;//enable shadow
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize( sceneWidth, sceneHeight );
+  // dom = document.getElementById('start_game');
+  dom = document.getElementById('game_ifrm')
+    .contentWindow
+    .document
+    .getElementById('cicciopiccio');
+  dom.appendChild(renderer.domElement);
+
+  var planeGeometry = new THREE.PlaneGeometry( 5, 5, 4, 4 );
+  var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+  ground = new THREE.Mesh( planeGeometry, planeMaterial );
+  ground.receiveShadow = true;
+  ground.castShadow=false;
+  ground.rotation.x=-Math.PI/2;
+  scene.add( ground );
+
+  camera.position.z = 5;
+  camera.position.y = 1;
+
+  sun = new THREE.DirectionalLight( 0xffffff, 0.8);
+  sun.position.set( 0,4,1 );
+  sun.castShadow = true;
+  scene.add(sun);
+  //Set up shadow properties for the sun light
+  sun.shadow.mapSize.width = 256;
+  sun.shadow.mapSize.height = 256;
+  sun.shadow.camera.near = 0.5;
+  sun.shadow.camera.far = 50 ;
+
+  orbitControl = new THREE.OrbitControls( camera, renderer.domElement );//helper to rotate around in scene
+  orbitControl.addEventListener( 'change', render );
+  //orbitControl.enableDamping = true;
+  //orbitControl.dampingFactor = 0.8;
+  orbitControl.enableZoom = false;
+
+  //var helper = new THREE.CameraHelper( sun.shadow.camera );
+  //scene.add( helper );// enable to see the light cone
+
+  window.addEventListener('resize', onWindowResize, false);//resize callback
+}
+
+function update(){
+  //animate
+  // hero.rotation.x += 0.01;
+  // hero.rotation.y += 0.01;
+  render();
+  requestAnimationFrame(update);//request next update
+}
+function render(){
+  renderer.render(scene, camera);//draw
+}
+function onWindowResize() {
+  //resize & align
+  sceneHeight = window.innerHeight;
+  sceneWidth = window.innerWidth;
+  renderer.setSize(sceneWidth, sceneHeight);
+  camera.aspect = sceneWidth/sceneHeight;
+  camera.updateProjectionMatrix();
 }
 
 function start_game() {
   document.getElementById("playBTN").style.display='none';
   document.getElementById("start_game").style.display='block';
   document.getElementById("game_ifrm").style.display='block';
+  init();
+  addTank();
 
 }
 
@@ -85,10 +122,10 @@ function addTank(){
       tank = obj;
       TANK_LOADED = true;
       NUM_LOADED++;
-      tank.position.z = -5.5;
-      tank.position.y = 0.58;
+      tank.position.x = 0;
+      tank.position.z = 0;
+      tank.position.y = 0;
       tank.scale.x = tank.scale.y = tank.scale.z =0.7;
-      car.is_ob = true;
       scene.add(obj);
 
       Box = scene.getObjectByName('Box');
@@ -134,11 +171,6 @@ function addTank(){
     function (err) {
       console.error('An error happened');
       TANK_LOADED = false;
-    }                );
-
-
+    });
 }
-
-init();
-loadScene();
 

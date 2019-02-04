4dcb3a1f678ec461a8f2c1445029801722f2020b
Paolo Mastrobuono Battisti <paolo@foneclay.com>
2019-02-04 15:08:56 +0100

    no message

--- BunkerBuster/src/js/chart_score.js
+++ BunkerBuster/src/js/chart_score.js
@@ -3,12 +3,15 @@ var score = 0;
 
 function save_high_score(score) {
   var highScore = localStorage.getItem("highScore") || 0;
+  var username = localStorage.getItem("username_id") || 0;
   if(highScore !== null){
     if (score > highScore) {
+      localStorage.setItem("username_id", username);
       localStorage.setItem("highScore", score);
     }
   }
   else{
     localStorage.setItem("highScore", score);
+    localStorage.setItem("username_id", username);
   }
 }
